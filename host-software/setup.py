ad685f56b5c6b7e68d2dce1f7262d2d922fb2652
ahtn <jem@seethis.link>
2018-02-07 23:56:54 +1000

    add placeholder python module

---
+++ host-software/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2018 jem@seethis.link
+# Licensed under the MIT license (http://opensource.org/licenses/MIT)
+
+from setuptools import setup
+
+setup(
+    name = 'keyplus',
+    version = '0.0.0',
+    description = "Python library for interfacing with keyplus keyboards.",
+    url = "http://github.com/ahtn/keyplus",
+    author = "jem",
+    author_email = "jem@seethis.link",
+    license = 'MIT',
+    packages = ['keyplus'],
+    install_requires = ['hexdump', 'intelhex', 'easyhid', 'pyyaml'],
+    keywords = ['keyboard', 'usb', 'hid'],
+    zip_safe = False
+)
