8f31d4d7c085b12efa1bcd294e66848c971f7931
ahtn <jem@seethis.link>
2018-05-19 19:34:53 +1000

    fix UID for efm8ub3

--- ports/efm8/efm8/efm8_util/uid.h
+++ ports/efm8/efm8/efm8_util/uid.h
@@ -5,35 +5,6 @@
 
 #include <stdint.h>
 
-#if defined(DOXYGEN)
-    /// The size in bytes of the EFM8 UID. (May vary by device)
-    #define EFM8_UID_SIZE
-
-    /// @brief EFM8 128 bit unique identifier (UID)
-    ///
-    /// Each EFM8 has a UID that is stored in either flash or ram at a fixed
-    /// address.
-    ///
-    /// On some chips this UID is a UUID, on others it is only a UID and does
-    /// not have the standard UUID format.
-    ///
-    /// * On EFM8UB1: 128bit UUID stored in flash
-    /// * On EFM8UB3: 128bit UUID stored in flash
-    /// * On EFM8UB2: 128bit UID stored in XRAM
-    ///
-    static const uint8_t EFM8_UID[EFM8_UID_SIZE];
-
-    /// Defined if the EFM8_UID is stored in XRAM
-    #define EFM8_UID_XRAM
-
-    /// Defined if the EFM8_UID is stored in flash
-    #define EFM8_UID_FLASH
-
-    /// Defined if the EFM8_UID is stored in internal RAM
-    #define EFM8_UID_RAM
-
-#endif
-
 // Universal Bee
 #if defined(EFM8UB1_DEVICE)
     #define EFM8_UID_SIZE 16
@@ -89,20 +60,45 @@
     #define EFM8_UID_RAM
 #endif
 
-// Variants that store the UID in flash
-#ifdef EFM8_UID_FLASH
-    #define EFM8_UID_ADDRESS   0xFFC0
-    static const __code __at(EFM8_UID_ADDRESS) uint8_t EFM8_UID[EFM8_UID_SIZE];
-#endif
 
-// Variants that store the UID in XRAM
-#ifdef EFM8_UID_XRAM
-    #define EFM8_UID_ADDRESS   (DEVICE_XRAM_SIZE - EFM8_UID_SIZE)
-    static const __xdata __at(EFM8_UID_ADDRESS) uint8_t EFM8_UID[EFM8_UID_SIZE];
-#endif
+#if defined(DOXYGEN)
+    /// The size in bytes of the EFM8 UID. (May vary by device)
+    #define EFM8_UID_SIZE
 
-// Variants that store the UID in internal RAM
-#ifdef EFM8_UID_RAM
-    #define EFM8_UID_ADDRESS   (256 - EFM8_UID_SIZE)
-    static const __idata __at(EFM8_UID_ADDRESS) uint8_t EFM8_UID[EFM8_UID_SIZE];
+    /// @brief EFM8 128 bit unique identifier (UID)
+    ///
+    /// Each EFM8 has a UID that is stored in either flash or ram at a fixed
+    /// address.
+    ///
+    /// On some chips this UID is a UUID, on others it is only a UID and does
+    /// not have the standard UUID format.
+    ///
+    /// * On EFM8UB1: 128bit UUID stored in flash
+    /// * On EFM8UB3: 128bit UUID stored in flash
+    /// * On EFM8UB2: 128bit UID stored in XRAM
+    ///
+    static const uint8_t EFM8_UID[EFM8_UID_SIZE];
+
+    /// Defined if the EFM8_UID is stored in XRAM
+    #define EFM8_UID_XRAM
+
+    /// Defined if the EFM8_UID is stored in flash
+    #define EFM8_UID_FLASH
+
+    /// Defined if the EFM8_UID is stored in internal RAM
+    #define EFM8_UID_RAM
+#else
+    // Variants that store the UID in flash
+    #if defined(EFM8_UID_FLASH)
+        #define EFM8_UID_ADDRESS   0xFFC0
+        static const __code __at(EFM8_UID_ADDRESS) uint8_t EFM8_UID[EFM8_UID_SIZE];
+    // Variants that store the UID in XRAM
+    #elif defined(EFM8_UID_XRAM)
+        #define EFM8_UID_ADDRESS   (DEVICE_XRAM_SIZE - EFM8_UID_SIZE)
+        static const __xdata __at(EFM8_UID_ADDRESS) uint8_t EFM8_UID[EFM8_UID_SIZE];
+    // Variants that store the UID in internal RAM
+    #elif defined(EFM8_UID_RAM)
+        #define EFM8_UID_ADDRESS   (256 - EFM8_UID_SIZE)
+        static const __idata __at(EFM8_UID_ADDRESS) uint8_t EFM8_UID[EFM8_UID_SIZE];
+    #endif
 #endif
