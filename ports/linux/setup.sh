8f7292d6a8d44b436abd2e858ef62065f372f8a1
ahtn <jem@seethis.link>
2019-06-12 23:01:33 +1000

    add make install opt to skip creating `keyplusd`

--- ports/linux/setup.sh
+++ ports/linux/setup.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-if ! id -u keyplusd 2>&1 > /dev/null; then
-    # Create the `keyplusd` user
+# Create the `keyplusd` user if it doesn't exist already
+if ! id -u keyplusd &> /dev/null; then
     useradd keyplusd \
         --comment "keyplus daemon user" \
         --system \
         --home-dir "/" \
         --groups input \
         --shell /sbin/nologin
-fi
-
-if [ "$1" == "no-install" ]; then
-    mkdir -p /usr/local/lib/udev/rules.d/
-    cp ./etc/udev/rules.d/85-keyplusd.rules -t /usr/local/lib/udev/rules.d/
 fi
